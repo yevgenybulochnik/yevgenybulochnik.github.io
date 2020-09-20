70754005d4d85c2b88cfad550a27a6073da9d1d7
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-09-20 11:38:22 -0700

    Basic setup for about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './about.scss'
 
 export default () => (
-  <div>
-    <p>React Static is a progressive static site generator for React.</p>
+  <div className='about'>
+    <p>
+      Hello! and welcome to my website. My name is Eugene Bulochnik. I am an avid computer programmer and clinical pharmacist, currently living in the beautiful pacific northwest (Portland, OR). Originally I grew up in a town called Eden prairie, close to Minneapolis Minnesota.
+    </p>
+    <p>
+      In 2012 I graduated from Drake University, with a Pharmacy Doctorate. I was accepted into the accelerated pharmacy program straight from highschool and completed my degree in 6 years. Currently, I hold my PharmD licence in the State of Oregon.
+    </p>
+    <p>
+      As a kid I was always interested in computers. However, it wasn’t until college that I really had my first taste of coding. During an Internal Medicine rotation I developed a diabetes tracking application using visual basic and excel.
+    </p>
+    <p>
+      Over the last 4 – 5 years I have started taking programming much more seriously. Whether its using ansible to provision servers or using typescript and react to develop an API/SPA, I am ecstatic. I find building something from scratch extremely gratifying and feel programming knowledge can be applied to almost any subject.
+    </p>
   </div>
 )
